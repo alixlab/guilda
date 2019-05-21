627c6440930359c9622836c0673851d29acefacc
carlaam <carla.am@outlook.com>
2019-05-21 13:32:50 -0300

    estruturação da pagina characters.html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = {
   races: firstDomain + 'races',
   classes: firstDomain + 'classes',
   magicItems: firstDomain + 'magicitems',
-  equipment: secondDomain + 'equipment'
+  equipment: secondDomain + 'equipment',
 };
 
 function getSpells() {
@@ -15,18 +15,65 @@ function getSpells() {
     console.log(error);
   });
 }
+
 function getMonsters() {
   return fetch(routes.monsters).then(response => response.json()).catch(error => {
     console.log(error);
   });
 }
 
+function centerModal() {
+  $(this).css('display', 'block');
+  let $dialog = $(this).find('.modal-dialog');
+  let offset = ($(window).height() - $dialog.height()) / 2;
+  // Center modal vertically in window
+  $dialog.css('margin-top', offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+
+$(window).on('resize', function() {
+  $('.modal:visible').each(centerModal);
+});
+
 async function displayMonsters() {
   let dataMonsters = await getMonsters();
-  console.log(dataMonsters);  
+  let character = document.querySelector('#character');
+  character.innerHTML = `
+   ${dataMonsters.results.map((monster) => `
+   <div class="bgcolor-item d-flex justify-content-end my-3">
+   <article class="p-2 d-flex flex-column">
+      <span class="name">${monster.name}</span>
+        <div class="d-flex">
+          <div class="info-block" >
+            <section class="pt-1 d-flex flex-column">
+              <span class="title"> TYPE</span>
+              <span class="description">${monster.type}</span>
+            </section>
+            <section class="pt-3 d-flex flex-column">
+              <span class="title"> ALIGNMENT</span>
+              <span class="description">${monster.alignment}</span>
+            </section>
+          </div>
+          <div class="info-block">
+            <section class="pt-1 d-flex flex-column">
+              <span class="title"> SIZE</span>
+              <span class="description">${monster.size}</span>
+            </section>
+            <section class="pt-3 d-flex flex-column">
+              <span class="title"> ENVIRONMENT</span>
+              <span class="description">${monster.environment}</span>
+            </section>
+          </div>
+        </div>
+    </article>
+    <picture class="picture-monster d-flex align-item-center">
+      <img class="monster img-fluid" srcset="/img/636288206263978085.png">
+    </picture>
+  </div>
+  `).join("")}
+  `
 }
-  
-displayMonsters();
 
 function getRaces() {
   return fetch(routes.races).then(response => response.json()).catch(error => {
