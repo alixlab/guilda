15658b5fa5075d06e2b771d59b0b2f55d4c54a50
alinemarjorie <aline.marjorie.af@gmail.com>
2019-05-21 15:55:56 -0300

    modal de detalhes adicionado

--- races.js
+++ races.js
@@ -16,7 +16,7 @@ function templateRaces(race) {
   $('#races').append(`
 <div class="bgcolor-item d-flex justify-content-end">
     <article class="p-2 d-flex flex-column">
-        <span class="name" id=${race.name}>${race.name}</span>
+        <span class="name" id=${race.name} data-toggle='modal' data-target='#exampleModalLong'>${race.name}</span>
         <div class="d-flex">
             <div class="info-block" >
                 <section>
@@ -32,14 +32,13 @@ function templateRaces(race) {
 `);
 
   $(`span[id=${race.name}]`).click(function() {
-    $('#races').empty();
     detailRace(race);
   });
 }
 
 function detailRace(race) {
   clearObj(race);
-  $('#races').append(`
+  $('.modal-body').html(`
     <div class="bgcolor-item d-flex justify-content-end">
       <article class="p-2 d-flex flex-column">
         <span class="name">${race.name}</span>
